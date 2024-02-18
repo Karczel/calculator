ca683992046cbf903e1917d9904c9bdf73548353
Karczel <92095164+Karczel@users.noreply.github.com>
2024-02-19 04:27:27 +0700

    Update calculator_ui.py

--- calculator_ui.py
+++ calculator_ui.py
@@ -1,8 +1,9 @@
 """Calculator UI class"""
 import tkinter as tk
-from keypad import Keypad
-from math import*
+from tkinter import ttk
 
+from keypad import Keypad
+from math import *
 
 
 class Calculator_UI(tk.Tk):
@@ -14,17 +15,32 @@ class Calculator_UI(tk.Tk):
 
     def init_components(self):
         self.output = tk.StringVar()
+        self.history = tk.StringVar()
 
         # Converter
         title = tk.Label(self, text="Converter", bg='black', fg='white', font=('Times New Roman', 50, 'normal'))
 
         # Display
-        self.display = tk.Label(self, text=self.output.get(), bg='white', justify="left", anchor='e', font=('Times New Roman', 50, 'normal'))
+        self.display = tk.Label(self, text=self.output.get(), bg='white', justify="left", anchor='e',
+                                font=('Times New Roman', 50, 'normal'))
 
         # keypads
         self.num_keypad = self.pad(list('789456123 0.'), 3)
         self.operator_keypad = self.pad(list('*/+-^=%()'), 1)
         self.other_keypad = self.pad(['DEL', 'CLR'], 1)
+
+        # functions
+        self.frame_f = tk.Frame(self)
+        self.function, self.functions_chooser = self.load_functions(['exp', 'ln', 'log10', 'log2', 'sqrt'])
+        self.add_function = tk.Button(self.frame_f, text='Add', command=self.handle_functions)
+        self.history_display = tk.Label(self.frame_f, text=self.output.get(), bg='white', justify="left", anchor='e',
+                                font=('Times New Roman', 20, 'normal'))
+
+
+        # Layout within frame
+        self.functions_chooser.pack()
+        self.add_function.pack()
+        self.history_display.pack()
 
         # bind
         self.num_keypad.bind(self.handle_digit)
@@ -37,9 +53,21 @@ class Calculator_UI(tk.Tk):
         self.num_keypad.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
         self.operator_keypad.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
         self.other_keypad.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        self.frame_f.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
 
     def pad(self, keynames, columns, *args):
         return Keypad(self, keynames=keynames, columns=columns)
+
+    def load_functions(self, lst):
+        """Load units of the requested unittype into the comboboxes."""
+        selected = tk.StringVar()
+        # put the unit names (strings) in the comboboxes
+        chooser = ttk.Combobox(self.frame_f, textvariable=selected)
+        # and select which unit to display
+        chooser['values'] = lst
+        chooser.current(newindex=0)
+        chooser.bind('<<ComboboxSelected>>')
+        return selected, chooser
 
     def handle_features(self, event):
         if event.widget.cget('text') == 'DEL':
@@ -49,6 +77,10 @@ class Calculator_UI(tk.Tk):
             self.output.set('')
             self.display.config(text=self.output.get())
 
+    def handle_functions(self):
+        self.output.set(self.output.get() + self.function.get())
+        self.display.config(text=self.output.get())
+
     def handle_digit(self, event):
         if event.widget.cget('text') == '=':
             output = self.output.get()
@@ -56,10 +88,16 @@ class Calculator_UI(tk.Tk):
             if output[len(output) - 1] not in op \
                     and output[len(output) - 1] not in ['.']:
                 self.display.config(fg='black')
+                prev = self.output.get()
                 try:
-
+                    # fix ^ to **
+                    # replace functions in string with math. , look up math docs
+                    # replace
                     self.output.set(eval(self.output.get()))
                     self.display.config(text=self.output.get())
+                    self.history.set(self.history.get()
+                                     + prev + ' = ' + self.output.get()+'\n')
+                    self.history_display.config(text=self.history.get())
                 except SyntaxError:
                     self.display.config(fg='red')
             else:
